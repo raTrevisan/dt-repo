a657f921182594dc82cb01b61492b5c61087ac15
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-03-22 18:48:30 +0000

    Agora funciona

--- commands/test5.sh
+++ commands/test5.sh
@@ -3,19 +3,24 @@ for i in 1 2 3 4 5
 do
 docker stack deploy dt-test_$i --compose-file docker-compose.yml
 sleep 5
-twin = 
-docker service scale "dt-test_"$i"_twin=0" "dt-test_"$1"_client=0"
+docker service scale "dt-test_"$i"_twin=0" "dt-test_"$i"_client=0"
 sleep 1
 docker service scale "dt-test_"$i"_twin=1"
-sleep 5
+sleep 10
 docker service scale "dt-test_"$i"_client=1"
-echo Operation Starting, it $i
-sleep 50
-echo "50%"
-sleep 50
+echo Operation Starting, iteration number $i
+sleep 20
+echo "20%"
+sleep 20
+echo "40%"
+sleep 20
+echo "60%"
+sleep 20
+echo "80%"
+sleep 20
 echo "done"
 mkdir res_$i
-cd res
+cd res_$i
 docker container ls --format {{.Names}} | xargs -I {} sh -c 'docker logs {} -t --details 2>&1 | tee {}.log'
 docker stack rm dt-test$i
 cd ..
