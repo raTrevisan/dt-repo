7a422009c62d2d38fde3cac8724ce3e408aa655e
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-04-14 06:42:17 -0300

    varios testes

--- commands/test5_4c_1b.sh
+++ commands/test5_4c_1b.sh
@@ -2,24 +2,21 @@
 for i in 1 2 3 4 5 
 do
 docker stack deploy dt-test_$i --compose-file docker-compose.yml
-sleep 5
+sleep 10
 docker service scale "dt-test_"$i"_twin=0" "dt-test_"$i"_client_a=0" "dt-test_"$i"_client_b=0" "dt-test_"$i"_client_c=0" "dt-test_"$i"_client_d=0"
-sleep 1
+sleep 10
 docker service scale "dt-test_"$i"_twin=1"
 sleep 10
 docker service scale "dt-test_"$i"_client_a=1" "dt-test_"$i"_client_b=1" "dt-test_"$i"_client_c=1" "dt-test_"$i"_client_d=1" 
 echo Operation Starting, iteration number $i
-sleep 50
-echo "20%"
-sleep 50
-echo "40%"
-sleep 50
-echo "60%"
-sleep 50
-echo "80%"
-sleep 50
-echo "done"
-sleep 50
+mkdir stats_$i
+cd stats_$i
+for j in $(seq 1 30) do
+echo saving stats
+docker stats --no-stream -> "stats_"$j
+sleep 5
+done
+cd ..
 mkdir res_$i
 cd res_$i
 docker container ls --format {{.Names}} | xargs -I {} sh -c 'docker logs {} -t --details 2>&1 | tee {}.log'
