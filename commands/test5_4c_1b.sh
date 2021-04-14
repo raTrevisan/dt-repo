c50fd4d06b93d88f838c609b182643a137252b91
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-04-14 13:24:06 -0300

    fix no script ?

--- commands/test5_4c_1b.sh
+++ commands/test5_4c_1b.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+docker stack deploy dt-test_$i --compose-file docker-compose.yml
 for i in 1 2 3 4 5 
 do
-docker stack deploy dt-test_$i --compose-file docker-compose.yml
 sleep 10
 docker service scale "dt-test_"$i"_twin=0" "dt-test_"$i"_client_a=0" "dt-test_"$i"_client_b=0" "dt-test_"$i"_client_c=0" "dt-test_"$i"_client_d=0"
 sleep 10
@@ -23,6 +23,6 @@ cd ..
 docker container ls --format {{.Names}} | xargs -I {} sh -c 'docker logs {} -t --details 2>&1 | tee {}.log'
 docker stats --no-stream > stats.log
 docker stack rm dt-test_$i
-sleep 5
+sleep 10
 cd ..
 done
