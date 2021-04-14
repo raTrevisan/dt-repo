577bb80203012759efb7e1502ca88f2b542c6601
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-04-14 16:41:58 +0000

    ???

--- commands/test5_4c_1b.sh
+++ commands/test5_4c_1b.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-$i = 1
-docker stack deploy dt-test_$i --compose-file docker-compose.yml
+
 for i in 1 2 3 4 5 
 do
+if [$i -eq 1]
+then
+docker stack deploy dt-test --compose-file docker-compose.yml
+fi
 sleep 10
 docker service scale "dt-test_"$i"_twin=0" "dt-test_"$i"_client_a=0" "dt-test_"$i"_client_b=0" "dt-test_"$i"_client_c=0" "dt-test_"$i"_client_d=0"
 sleep 10
@@ -14,7 +17,7 @@ mkdir res_$i
 cd res_$i
 mkdir stats_$i
 cd stats_$i
-for j in $(seq 1 30) 
+for j in $(seq 1 20) 
 do
 echo saving stats
 docker stats --no-stream > "stats_"$j
