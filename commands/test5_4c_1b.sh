f70464dc6f642935c0337b72dba901b217113912
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-04-14 15:17:59 -0300

    meu deus me ajuda

--- commands/test5_4c_1b.sh
+++ commands/test5_4c_1b.sh
@@ -6,14 +6,14 @@ sleep 10
 docker service scale "dt-test_"$i"_twin=0" "dt-test_"$i"_client_a=0" "dt-test_"$i"_client_b=0" "dt-test_"$i"_client_c=0" "dt-test_"$i"_client_d=0"
 sleep 10
 docker service scale "dt-test_"$i"_twin=1"
-sleep 10
+sleep 15
 docker service scale "dt-test_"$i"_client_a=1" "dt-test_"$i"_client_b=1" "dt-test_"$i"_client_c=1" "dt-test_"$i"_client_d=1" 
 echo Operation Starting, iteration number $i
 mkdir res_$i
 cd res_$i
 mkdir stats_$i
 cd stats_$i
-for j in $(seq 1 20) 
+for j in $(seq 1 17) 
 do
 echo saving stats
 docker stats --no-stream > "stats_"$j".log"
